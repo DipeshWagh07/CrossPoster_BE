cc0e28998d0070120b1f9fb0be713ecc4ea9a005
Sandhya Mhetre <sandhya.mhetre@techsierra.in>
2025-07-18 21:17:02 +0530

    i have made changes here const redirectUri = https://cross-poster-fe.vercel.app/auth/twitter/callback

--- utils/twitterXAuth.js
+++ utils/twitterXAuth.js
@@ -4,7 +4,7 @@ import fs from "fs";
 
 const TWITTER_API_KEY = "SbR5iyyJcLPoZP5nx27gWAqBo";
 const TWITTER_API_SECRET = "59nbvNET1M2cWuztQ02gR6FnJcpcNC7ZWSHDTe9QcoQfRh3XGE";
-const redirectUri = "https://crossposter-be.onrender.com/auth/twitter/callback";
+const redirectUri = "https://cross-poster-fe.vercel.app/auth/twitter/callback";
 
 // Get OAuth authentication URL
 export const getAuthUrl = async () => {
